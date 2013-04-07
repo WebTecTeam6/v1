fb48e4f9188c724a59b3fc37f6cfb6d4fc9fe2ce
Viktor Zener <viktor.zener@googlemail.com>
2013-04-07 09:49:52 +0200

    fixed style on app_waypoint.php

--- site/app_waypoint.php
+++ site/app_waypoint.php
@@ -14,6 +14,7 @@
     <link href="../bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="../bootstrap/css/docs.css" rel="stylesheet">
     <link href="../bootstrap/css/footer.less" rel="stylesheet/less" type="text/css">
+    <link href="../bootstrap/css/content.less" rel="stylesheet/less" type="text/css">
 
     <title>Seapal</title>
 
