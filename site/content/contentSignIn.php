b01c8e233cd9407d9827acf7d9a73d41e7001217
nubatz <H4rLek1N@gmx.de>
2013-04-18 12:24:57 +0200

    ><(((°< *auslesen aus DB und in TF eintragen*

--- site/content/contentSignIn.php
+++ site/content/contentSignIn.php
@@ -10,12 +10,49 @@
         <h2>Sign In</h2>
     </div>
 
+    <?php
+    $firstName = $_POST['firstname'];
+    $lastName = $_POST['lastname'];
+    $age = $_POST['age'];
+    $con = mysql_connect("localhost","root","gras17");
+    mysql_select_db("webtec");
+    $sql_firstName = "SELECT FirstName FROM Persons";
+    $sql_lastName = "SELECT LastName FROM Persons";
+    $sql_age = "SELECT age FROM Persons";
+
+    $db_erg_firstName = mysql_query( $sql_firstName);
+    $db_erg_lastName = mysql_query( $sql_lastName);
+    $db_erg_age = mysql_query( $sql_age);
+    if ( (! $db_erg_firstName) || (! $db_erg_lastName) || (! $db_erg_age) )
+    {
+        die('Ungültige Abfrage: ' . mysql_error());
+    }
+
+    echo $_POST['firstname'];
+    echo $_POST['lastname'];
+    echo $_POST['age'];
+
+    mysql_select_db("webTec", $con);
+
+    $sql="INSERT INTO Persons (FirstName, LastName, Age) VALUES('$firstName', '$lastName', '$age')";
+
+    if (!mysql_query($sql,$con))
+    {
+        die('Error: ' . mysql_error());
+    };
+
+    ?>
+
     <div>
         <p>
-        <form action="insert.php" method="post">
-            Firstname: <input type="text" name="firstname" />
-            Lastname: <input type="text" name="lastname" />
-            Age: <input type="text" name="age" />
+        <form action="" method="post">
+            Firstname: <input value="<?php
+                if (isset($_POST['firstname'])){
+                    echo $_POST['firstname'];
+                }
+            ?>" id="firstName" type="text" name="firstname" />
+            Lastname: <input id="lastName" type="text" name="lastname" />
+            Age: <input id="age" type="text" name="age" />
             <input type="submit" />
         </form></p>
 
--- site/insert.php
+++ site/insert.php
@@ -10,6 +10,9 @@
 echo $_POST['firstname'];
 echo $_POST['lastname'];
 echo $_POST['age'];
+$firstName = $_POST['firstname'];
+$lastName = $_POST['lastname'];
+$age = $_POST['age'];
 
 $con = mysql_connect("localhost","root","gras17");
 if (!$con){
@@ -18,11 +21,23 @@ if (!$con){
 
 mysql_select_db("webTec", $con);
 
-$sql="INSERT INTO Persons (FirstName, LastName, Age) VALUES('$_POST[firstname]', '$_POST[lastname]', '$_POST[age]')";
+$sql="INSERT INTO Persons (FirstName, LastName, Age) VALUES('$firstName', '$lastName', '$age')";
 
 if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
   };
+
+$sql_firstName = "SELECT FirstName FROM Persons";
+$sql_lastName = "SELECT LastName FROM Persons";
+$sql_age = "SELECT age FROM Persons";
+
+$db_erg = mysql_query( $sql );
+if ( ! $db_erg )
+{
+    die('Ungültige Abfrage: ' . mysql_error());
+}
+
+
 
 ?>
